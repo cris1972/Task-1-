8f14b26918e6333dcae2c4f892babdfa56ef3705
Евгений Капустянский <evgenijkapustanskij@Evgenij-MacBook-Pro.local>
2025-04-10 10:47:03 +0300

    Реализация JDBC: UserDao, UserService, таблица users, все тесты проходят

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,31 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util.getConnection();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        List<User> users = new ArrayList<>();
+        users.add(new User("Иван", "Иванов", (byte) 20));
+        users.add(new User("Александр", "Петров", (byte) 45));
+        users.add(new User("Сергей", "Сидоров", (byte) 34));
+        users.add(new User("Константин", "Смирнов", (byte) 20));
+        for (User user : users) {
+            userService.saveUser(user.getName(), user.getLastName(), user.getAge());
+            System.out.println("User с именем – " + user.getName() + " добавлен в базу данных");
+        }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
