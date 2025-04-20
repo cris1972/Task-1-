31c761497b4401b3f2701f4ed717be5456c3d3cd
sv-dan <sav4enkov.d@mail.ru>
2025-04-20 14:19:55 +0300

    task 1.1.3 JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserDaoJDBCImpl dao = new UserDaoJDBCImpl();
+        dao.createUsersTable();
+        System.out.println();
+        dao.saveUser("Петя", "Бобров", (byte) 69);
+        dao.saveUser("Маша", "Сидорова", (byte) 52);
+        dao.saveUser("Евгений", "Михайлов", (byte) 41);
+        dao.saveUser("Дима", "Дубров", (byte) 19);
+        System.out.println();
+        dao.getAllUsers();
+        System.out.println();
+        dao.cleanUsersTable();
+        System.out.println();
+        dao.dropUsersTable();
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+
     // реализуйте настройку соеденения с БД
-}
+    private static String url = "jdbc:postgresql://localhost:5432/postgres";
+    private static String user = "postgres";
+    private static String password = "zadedi92";
+
+    public static Connection getConnection() throws SQLException {
+        Connection connection = DriverManager.getConnection(url, user, password);
+            if (connection != null) {
+                System.out.println("Подключение к postgreSQL");
+            } else {
+                System.out.println("Ошибка подключения к postgreSQL");
+            }
+            return connection;
+        }
+    }
